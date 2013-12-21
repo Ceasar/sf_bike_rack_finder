8e44031c38ff66c775a5eca29b9fcefd3dee42cd
Ceasar Bautista <cbautista2010@gmail.com>
2013-12-20 22:41:41 -0500

    style changes

--- app/static/parkingSpots.js
+++ app/static/parkingSpots.js
@@ -1,7 +1,11 @@
 var SF = new google.maps.LatLng(37.783333, -122.416667);
-var directionsDisplay;
+
 var directionsService = new google.maps.DirectionsService();
+var distanceMatrixService = new google.maps.DistanceMatrixService();
+
+var directionsDisplay;
 var map;
+var markers = {};
 
 var initialize = function() {
     directionsDisplay = new google.maps.DirectionsRenderer();
@@ -11,17 +15,20 @@ var initialize = function() {
     };
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
     directionsDisplay.setMap(map);
-    showClosest(SF);
-    // todo: use user's current position in deploy
-    // navigator.geolocation.getCurrentPosition(function(position){
-    //  var lat = position.coords.latitude;
-    //  var lon = position.coords.longitude;
-    //  var myLocation = new google.maps.LatLng(lat, lon);
-    /// showClosest(myLocation);
-    //});
+    var target = SF;
+    getNearbySpots(target, function(parkingSpots) {
+        parkingSpots.forEach(function(spot) {
+            showSpot(spot);
+        });
+        getClosestSpot(parkingSpots, function(spot) {
+            showDirections(target, spot);
+            // todo: this doesn't work, probably because async
+            // markers[spot].setAnimation(google.maps.Animation.BOUNCE);
+        });
+    });
 };
 
-function calcRoute(start, end) {
+function showDirections(start, end) {
     var request = {
         origin: start,
         destination: end,
@@ -34,31 +41,32 @@ function calcRoute(start, end) {
     });
 }
 
-function showClosest(latLng) {
+function showSpot(spot) {
+    var position = new google.maps.LatLng(
+        spot.latitude,
+        spot.longitude
+    );
+    var marker = new google.maps.Marker({
+        position: position,
+        map: map,
+        title: spot['location'],
+    });
+    markers[spot] = marker;
+}
+
+function getClosestSpot(parkingSpots, success) {
+    var closestSpot = new google.maps.LatLng(
+        parkingSpots[0].latitude,
+        parkingSpots[0].longitude
+    );
+    success(closestSpot);
+}
+
+function getNearbySpots(latLng, success) {
     $.getJSON("/closest", {
         latitude: latLng.lat(),
         longitude: latLng.lng(),
-    }, function(parkingSpots) {
-        var closestSpot = new google.maps.LatLng(
-            parkingSpots[0].latitude,
-            parkingSpots[0].longitude
-            );
-        var markers = [];
-        parkingSpots.forEach(function(spot) {
-            var position = new google.maps.LatLng(
-                spot.latitude,
-                spot.longitude
-                );
-            var marker = new google.maps.Marker({
-                position: position,
-                map: map,
-                title: spot['location'],
-            });
-            markers.push(marker);
-        });
-        markers[0].setAnimation(google.maps.Animation.BOUNCE);
-        calcRoute(latLng, closestSpot);
-    }
+    }, success
     );
 }
 
