01f15854c9de0dab8c0bb57a7d6f1e6046e0dea5
Ceasar Bautista <cbautista2010@gmail.com>
2013-12-20 20:03:17 -0500

    prototype displaying data

--- app/views/general.py
+++ app/views/general.py
@@ -1,8 +1,19 @@
+import json
+
 from flask import render_template
+import requests
 
 from app import app
 
 
+SF_BIKE_PARKING_API_ENDPOINT = "http://data.sfgov.org/resource/w969-5mn4.json"
+
+
+def get_rows():
+    with open("app/data/parking_spots.json") as f:
+        return json.loads(f.read())
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    rows = get_rows()
+    return render_template("index.html", rows=rows)
